4c29b27c646e1693b85d3d691d8787642e6b3393
630clx <1179426016@qq.com>
2013-03-14 09:22:37 +0800

    menu.php

---
+++ menu.php
@@ -0,0 +1,15 @@
+<!-- 每个php文件都要加上这个menu.php导航条，用法请参考index.php -->
+
+<header id="menu" class="menuOff" onMouseOver="headerMouseOver();" onMouseOut="headerMouseOut();">
+	<nav>
+		<ul>
+			<li></li>
+			<li class="nav-index"><a href="">首页</a></li>
+			<li class="nav-atme"><a href="">@提到我的</a></li>
+			<li class="nav-comment"><a href="">评论</a></li>
+			<li class="nav-collect"><a href="">收藏</a></li>
+			<li class="nav-acount"><a href="">账号设置</a></li>
+			<li class="nav-log"><a href="">登录</a></li>
+		</ul>
+	</nav>
+</header>
